16fcf7dba100068983b81a19c27f8e614e4e42f2
juan <curlydoge107@gmail.com>
2023-11-09 12:08:48 -0400

    getters and setter

---
+++ src/diccionario2.java
@@ -0,0 +1,28 @@
+public class diccionario2 {
+    String nombre;
+    String valor;
+
+    public diccionario2() {
+
+    }
+    public diccionario2(String nombre, String valor) {
+        this.nombre = nombre;
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+}
