1812dacb2e2bb6a9c0849d2ab96569cd9fc2172c
juan <Curlydoge107@gmail.com>
2023-11-10 11:41:26 -0400

    getters_setters añadiendo las opciones de figuras

---
+++ src/getters_setters.java
@@ -0,0 +1,66 @@
+public class getters_setters {
+    String nombre;
+    String valor;
+
+    Float area;
+    Float perimetro;
+    Float altura;
+
+    public Float getArea() {
+        return area;
+    }
+
+    public void setArea(Float area) {
+        this.area = area;
+    }
+
+    public Float getPerimetro() {
+        return perimetro;
+    }
+
+    public void setPerimetro(Float perimetro) {
+        this.perimetro = perimetro;
+    }
+
+    public Float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Float altura) {
+        this.altura = altura;
+    }
+
+    public Float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(Float radio) {
+        this.radio = radio;
+    }
+
+    Float radio;
+
+    public getters_setters() {
+
+    }
+    public getters_setters(String nombre, String valor) {
+        this.nombre = nombre;
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+}
