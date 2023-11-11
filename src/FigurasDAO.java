9eb54dab23fdf33aa39429b2713d136b81675285
XhantheProg <curlydoge107@gmail.com>
2023-11-11 19:12:25 -0400

    FigurasDAO se requiere una revision extra para las opciones (lo que añadi fue poner las opciones de añadir figura + poniendo los parametros de la opcion guardarCuadrado) despues buscar, borrar no estas hechas aun

--- src/FigurasDAO.java
+++ src/FigurasDAO.java
@@ -4,7 +4,74 @@ import java.sql.SQLException;
 
 public class FigurasDAO {
 
-    public static void guardarCalculoCilindro(getters_setters figuras) {
+    public static void guardarCuadradoEnDB(getters_setters figura) {
+        Connexion dbConexion = new Connexion();
+        try (Connection conexion = dbConexion.get_conConnection()) {
+            PreparedStatement ps= null;
+            try {
+                String query = "INSERT INTO `menuregis`.`figuras` (`lado`) VALUES (?)";
+                ps = conexion.prepareStatement(query);
+                ps.setFloat(1, figura.getLado());
+                ps.executeUpdate();
+                System.out.println("Figura guardada exitosamente en la base de datos.");
+
+            } catch (SQLException e) {
+                System.out.println(e);
+                System.out.println("No se pudo guardar la figura en la base de datos.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void guardarCilindroEnDB(double radio, double altura) {
+        double area = 2 * Math.PI * radio * (radio + altura);
+        double perimetro = 2 * Math.PI * radio;
+
+        String figura = "Cilindro - Área: " + area + ", Perímetro: " + perimetro;
+        guardarFiguraEnDB(figura);
+        System.out.println("Cilindro añadido exitosamente.");
+    }
+
+    public static void guardarConoEnDB(double radio, double altura) {
+        double area = Math.PI * radio * (radio + Math.sqrt(Math.pow(altura, 2) + Math.pow(radio, 2)));
+        double perimetro = 2 * Math.PI * radio;
+
+        String figura = "Cono - Área: " + area + ", Perímetro: " + perimetro;
+        guardarFiguraEnDB(figura);
+        System.out.println("Cono añadido exitosamente.");
+    }
+
+    public static void guardarEsferaEnDB(double radio) {
+        double area = 4 * Math.PI * Math.pow(radio, 2);
+        String figura = "Esfera - Área: " + area;
+
+        guardarFiguraEnDB(figura);
+        System.out.println("Esfera añadida exitosamente.");
+    }
+
+    public static void guardarFiguraEnDB(String figura) {
+        Connexion dbConexion = new Connexion();
+        try (Connection conexion = dbConexion.get_conConnection()) {
+            try {
+                String query = "INSERT INTO `menuregis`.`figuras` (`cuadrado`, `cono`, `cuadrado`, `esfera`, `area`, `perimetro`, `redio`) VALUES (?, ?, ?, ?, ?, ? ,?)";
+                PreparedStatement ps = conexion.prepareStatement(query);
+                ps.setString(1, figura);
+                ps.executeUpdate();
+                System.out.println("Figura guardada exitosamente en la base de datos.");
+
+            } catch (SQLException e) {
+                System.out.println(e);
+                System.out.println("No se pudo guardar la figura en la base de datos.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+
+
+    /*public static void guardarCalculoCilindro(getters_setters figuras) {
         Connexion db_conexion = new Connexion();
 
         try (Connection conexion = db_conexion.get_conConnection()) {
@@ -25,6 +92,6 @@ public class FigurasDAO {
         } catch (SQLException ex) {
             System.out.println(ex);
         }
-    }
+    }*/
     //
 }
